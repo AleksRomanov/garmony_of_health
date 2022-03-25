43d9822374bef93f8f583d1ca2bc12df6232ca3e
aleksromanov <kosmonavt-vavilon@mail.ru>
2022-03-26 00:43:37 +0700

    garmony_of_health_WIP render Cards ввод data search(regexp on input) setSearch with entering values User

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import {Card, Col, Image, Input, Row} from "antd";
 import {nanoid} from "@reduxjs/toolkit";
 import styled from "styled-components";
 import {fromEvent} from "rxjs";
-import {map, debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators'
+import {map, debounceTime, distinctUntilChanged, switchMap, tap} from 'rxjs/operators'
 import Text from "antd/lib/typography/Text";
 import Search from "antd/lib/input/Search";
 import {useEffect, useState} from "react";
@@ -40,38 +40,59 @@ const search = () => {
 
 
 export default function Index({data}) {
-    // const [searchByAuthorInput, setSearchByAuthorInput] = useState([]);
-    // const [searchByTitleInput, setSearchByTitleInput] = useState([]);
+    const [searchResult, setSearchResult] = useState([]);
+    const [searchStream, setSearchStream] = useState(false);
+
     const parseData = (value) => {
-        const matchData = (book) => {
-            // console.log(book.title.match(RegExp(`${value}`)));
-            return !!book.title.match(RegExp(`${value}`))
+        const matchTitle = (book) => {
+            return !!book.title.toLowerCase().match(RegExp(`${value.toLowerCase()}`))
         }
-        return data.bookList.filter(book => matchData(book) === true);
+        const matchAuthor = (book) => {
+            return !!book.author.toLowerCase().match(RegExp(`${value.toLowerCase()}`))
+        }
+        let titleList = data.bookList.filter(book => matchTitle(book) === true);
+        let authorList = data.bookList.filter(book => matchAuthor(book) === true);
+        return titleList.concat(authorList);
     }
 
     let search = undefined;
+
     useEffect(() => {
         if (!search) {
+            setSearchResult(document.getElementById('result'))
             search = document.getElementById('search');
 
-            const stream$ = fromEvent(search, 'input')
-                .pipe(
-                    map(event => event.target.value),
-                    debounceTime(1000),
-                    distinctUntilChanged(),
-                    // switchMap(value => {data.value}),
-                    switchMap(value => parseData(value))
-                )
 
-            stream$.subscribe(value => {
-                console.log(value)
-            })
+            if (!searchStream) {
+                let stream$ = fromEvent(search, 'input')
+                    .pipe(
+                        map(event => event.target.value),
+                        debounceTime(1000),
+                        distinctUntilChanged(),
+                        switchMap(value => parseData(value)),
+                        tap(() => searchResult.innerHTML = ''),
+                    );
+                setSearchStream(stream$);
+
+            }
         }
-
-
     }, [])
 
+    useEffect(() => {
+        if (searchStream) {
+            searchStream.subscribe(book => {
+                const html = `
+                <div class="card">
+                  <div class="card-image">
+                    <img src="${book.img}" />
+                    <span class="card-title">${book.title}</span>
+                  </div>
+                </div>
+            `;
+                searchResult.insertAdjacentHTML('beforeend', html)
+            })
+        }
+    }, [searchStream])
 
     function renderBook(book) {
         return (
@@ -87,17 +108,15 @@ export default function Index({data}) {
         );
     }
 
-    // const {Search} = Input;
-    // const onSearch = value => console.log(value);
-
     return (
         <MainLayout class="container" title={'Page Index'}>
             {/*<h1 style={{display: "flex", justifyContent: "center", opacity: "0", }}>The World Bestsellers</h1>*/}
             <Text style={{display: "flex", justifyContent: "center", fontSize: "4.2em", marginBottom: "30px"}} mark>The World Bestsellers</Text>
             {/*<Search id="search" placeholder="Поиск по автору или названию книги" allowClear onSearch={onSearch} style={{width: "100%"}}/>*/}
             <Search id="search" placeholder="Поиск по автору или названию книги" allowClear style={{width: "100%"}}/>
+            <div id="result"></div>
 
-            {/*<div style={{width: "50%", margin: "0 auto", paddingBottom: "30px"}} className="input-field">*/}
+            {/*<div style={{width: "50%", margins: "0 auto", paddingBottom: "30px"}} className="input-field">*/}
             {/*    <input type="text" id="search" onSearch={onSearch}/>*/}
             {/*    <label style={{}} htmlFor="search">Поиск по автору или названию книги</label>*/}
             {/*</div>*/}
